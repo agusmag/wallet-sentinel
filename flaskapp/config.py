11c93d0235a7a42d15030450eb9053dd31885447
Agustin Magliano <agustin-@live.com>
2020-01-02 00:02:08 -0300

    Delete migration image from .travis.yml and Dockerrun.aws.json

--- flaskapp/config.py
+++ flaskapp/config.py
@@ -6,18 +6,18 @@ class BaseConfig:
 
 class DevConfig(BaseConfig):
    DEBUG = True
-   SECRET_KEY = "{0}dev".format(os.environ.get('SECRET_KEY'))
+   SECRET_KEY = "{0}".format(os.environ.get('SECRET_KEY'))
    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://{0}:{1}@{2}/{3}'.format(os.environ.get('MYSQL_USER'), os.environ.get('MYSQL_PASSWORD'), os.environ.get('MYSQL_HOST'), os.environ.get('MYSQL_DATABASE'))
    SQLALCHEMY_TRACK_MODIFICATIONS = True
 
 class ProductionConfig(BaseConfig):
-   SECRET_KEY = "{0}prod".format(os.environ.get('SECRET_KEY'))
+   SECRET_KEY = "{0}".format(os.environ.get('SECRET_KEY'))
    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://{0}:{1}@{2}/{3}'.format(os.environ.get('MYSQL_USER'), os.environ.get('MYSQL_PASSWORD'), os.environ.get('MYSQL_HOST'), os.environ.get('MYSQL_DATABASE'))
    SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 class TestConfig(BaseConfig):
    TESTING = True
    DEBUG = True
-   SECRET_KEY = "{0}test".format(os.environ.get('SECRET_KEY'))
+   SECRET_KEY = "{0}".format(os.environ.get('SECRET_KEY'))
    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://agustinmag:password@localhost/wallet_sentinel_db_dev'
    SQLALCHEMY_TRACK_MODIFICATIONS = True
